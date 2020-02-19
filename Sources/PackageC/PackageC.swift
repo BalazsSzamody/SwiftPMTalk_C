4ffbdc7b61720acb9955530a4ae9dc65eeed38b5
Balazs Szamody <bszamody@gmail.com>
2020-02-19 12:38:52 +1100

    Library updated
    Fix

--- Sources/PackageC/PackageC.swift
+++ Sources/PackageC/PackageC.swift
@@ -1,3 +1,13 @@
-struct PackageC {
-    var text = "Hello, World!"
+import SwiftUI
+import PackageA
+
+public struct PackageC {
+    public struct CustomText: View {
+        let source = PackageA()
+        public var body: some View {
+            Text(source.text)
+        }
+        
+        public init() {}
+    }
 }
